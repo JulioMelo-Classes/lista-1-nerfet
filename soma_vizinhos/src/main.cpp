e5ba09a4fb93f9e0f38af495afe6e2cd1a540d22
nerfet <ric.alex55@yahoo.com>
2021-06-10 14:40:26 -0300

    Update main.cpp

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -10,7 +10,22 @@ using std::endl;
 
 int main( void )
 {
-    // TODO: Adicione seu código aqui. 
+  int num1, num2;
+  int somaTotal=0;
+  
+  cin >> num1;
+  cin >> num2;
 
+  if(num2 >=0){
+    for(int i=num1; i<num2+num1; i++){
+      somaTotal += i;
+    }
+  }else{
+    for(int i=num1; i>num2+num1; i--){
+      somaTotal += i;
+    }
+  }
+  
+  cout << somaTotal;
     return 0;
 }
