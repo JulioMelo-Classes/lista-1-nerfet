c0979bed7855cb73a469bbd018cc8f8972c09663
nerfet <ric.alex55@yahoo.com>
2021-06-12 19:45:08 -0300

    Update main.cpp

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -15,7 +15,40 @@ using std::setprecision;
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
+    int num;
+    float numOut=0, numTotal=0;
+    float num25=0, num50=0, num75=0, num100=0;
+
+    while( cin >> num) {
+        if(num>=0 && num<25){
+            num25++;
+        } 
+        if(num>=25 && num <50){
+            num50++;
+        } 
+        if(num>=50 && num <75){
+            num75++;
+        }
+        if(num>=75 && num <100){
+            num100++;
+        }
+        if(num<0 || num >=100){
+            numOut++;
+        }
+    }
+
+    numTotal = numOut + num25 + num50 + num75 + num100;
+
+    cout << endl;
+    cout << std::setprecision(4) << ((num25/numTotal)*100);
+    cout << endl;
+    cout << std::setprecision(4) << ((num50/numTotal)*100);
+    cout << endl;
+    cout << std::setprecision(4) << ((num75/numTotal)*100);
+    cout << endl;
+    cout << std::setprecision(4) << ((num100/numTotal)*100);
+    cout << endl;
+    cout << std::setprecision(4) << ((numOut/numTotal)*100);
 
     return 0;
 }
