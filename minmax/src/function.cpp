8a8d82a2a52948b439bfa045522dfb1158b53e65
nerfet <ric.alex55@yahoo.com>
2021-06-27 20:22:49 -0300

    Update function.cpp

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -13,7 +13,37 @@
 std::pair<int,int> min_max( int V[], size_t n )
 {
     // TODO: Adicione aqui sua solução.
+    int min=0, max=0;
+    int minPos=0, maxPos=0;
+    if(n=1){
+        return std::make_pair(minPos, maxPos);
+    }else if(n=0){
+        minPos=-1;
+        maxPos=-1;
+        return std::make_pair(minPos, maxPos);
+    }
+    if(V[0] > V[1]){
+        max=V[0];
+        maxPos=0;
+        min=V[1];
+        minPos=1;
+    }else{
+        max=V[1];
+        maxPos=1;
+        min=V[0];
+        minPos=0;
+    }
+    for(int i=2; i<n;i++){
+        if(V[i]<min){
+            min=V[i];
+            minPos=i;
+        }
+        if(V[i]>=max){
+            max=V[i];
+            maxPos=i;
+        }
+    }
 
     // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    return std::make_pair(minPos, maxPos);
 }
